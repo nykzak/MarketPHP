6c80562fa42f3ce9f95905647df55b8c98b2957e
Sergey <nykzak1@gmail.com>
2022-10-11 14:12:46 +0300

    Final

---
+++ config/params.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'admin_email' => 'nykzak1@gmail.com',
+    'shop_name' => 'GreySHOP',
+    'pagination' => 3,
+    'smtp_host' => 'smtp.mail.ru',
+    'smtp_port' => '465',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'nicols1234@mail.ru',
+    'smtp_password' => 'S8qNCdDNfzzi68hR2nWZ',
+    'img_width' => 125,
+    'img_height' => 200,
+    'gallery_width' => 700,
+    'gallery_height' => 1000,
+];
